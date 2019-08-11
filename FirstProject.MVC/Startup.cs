f398be14ef44e05eac1c7fe8cfbd1bf01955fd99
MariusA <anca.ticau@gmail.com>
2019-08-11 22:26:25 +0300

    initial commit CRUD(without delete) for categories, suppliers and shippers-MyStore

---
+++ FirstProject.MVC/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyStore.Data;
+using MyStore.Data.Repositories;
+using MyStore.Domain.Entities;
+using MyStore.Services;
+
+namespace FirstProject.MVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddDbContext<StoreContext>();
+            services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IShipperService, ShipperService>();
+            services.AddTransient<IShipperRepository, ShipperRepository>();
+            services.AddTransient<ISupplierService, SupplierService>();
+            services.AddTransient<ISupplierRepository, SupplierRepository>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ MyStore.Services/CustomerService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyStore.Data;
+using MyStore.Data.Repositories;
+using MyStore.Domain.Entities;
+
+namespace MyStore.Services
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customers> GetAllCustomers();
+        Customers FindMyCustomerById(int id);
+
+        Customers UpdateCustomer(Customers customerToUpdate);
+
+        Customers AddCustomer(Customers addedCustomer);
+
+       
+    }
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public IEnumerable<Customers> GetAllCustomers()
+        {
+            return customerRepository.GetAllCustomers();
+        }
+
+        public Customers FindMyCustomerById(int id)
+        {
+            var myCustomer = customerRepository.GetCustomerById(id);
+
+            if (myCustomer != null)
+            {
+                return myCustomer;
+            }
+
+            return null;
+        }
+
+        public Customers UpdateCustomer(Customers customerToUpdate)
+        {//id wverything ok, update the customer in db
+
+            return customerRepository.Update(customerToUpdate);
+        }
+
+        public Customers AddCustomer(Customers addedCustomer)
+        {
+            if (IsUniqueCompany(addedCustomer.Companyname))
+            {
+                return customerRepository.AddCustomer(addedCustomer);
+            }
+            return null;
+        }
+
+        private bool IsUniqueCompany(string name)
+        {
+            return customerRepository.IsUniqueCompanyName(name);
+        }
+    }
+}
